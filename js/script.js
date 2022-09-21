076c3341223d5d8ac325a2755169612470c03a7b
Lívia Émille Ferreira da Silva <livia_emille@hotmail.com>
2022-09-21 12:38:07 -0300

    alterações feitas para melhorar redimensionamento de imagens e formatação de texto, bem como adição de sombreamento em algumas divs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 function time(){
-    const d = new Date();     
+    start()
 
+    $(".logotransicao").animate({width: "30%"}, 1500, function(){
+        $(".blocotransicao").fadeOut(700)
+    });
+
+    const d = new Date();  
+
+    //Dia da semana por extenso em portugues
     var dias = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
     dias = dias[d.getDay() % 7];
+    
+    //Data em formato brasileiro
     data = d.toLocaleDateString();
+
+    //Horas, minutos e segundos
     hora = d.getHours();
     minuto = d.getMinutes();
     segundo = d.getSeconds();
 
+    //Formatação de horas, segundos e minutos para atribuir 0 a esquerda de cada um caso menores que 10
     if(hora < 10){
         hora="0"+hora;
     }
@@ -24,30 +36,31 @@ function time(){
     setTimeout('time()', 500);
 }
 
-
-$(document).ready(function(){
-    var urlimagerss = "img/logoif.png";
-    $("#logotransicao").animate({width: "30%"}, 1500, function(){
-        $("#blocotransicao").fadeOut(700)
+//Função para procurar publicações dentro do body e iniciar animações e formatação de texto
+function start(){
+    $('body').find('.post').each(function(){
+    var div = this;
+    iniciarAnimation($(div));
     });
-    $("#baackgoundrss").css('background-image', 'url(' + urlimagerss +')').promise().done(function(){
-        $(this).animate({
-            opacity: 1
-        }, 600)
+}
+
+//Enviando os parametros certos para a função replace_text
+function iniciarAnimation(div){
+    div.show();
+
+    //twitter
+    div.find('.text').each(function(){
+        replace_text(this,false);
     });
-});
 
+    //instagram
+    div.find('.textopost').each(function(){
+        replace_text(this,true);
+    });
+}
 
-
-    /*var bloco = $("#blocotransicao");
-    bloco.fadeOut(3000);
-        {top: "100%"}, 2500, function(){
-        $(this).hide()
-        }
-    );*/
-
-
-/*function replace_text(div,instagram){
+//Função para pegar conteúdo texto dos posts e enviar para as respectivas funções de formatação
+function replace_text(div,instagram){
     var curInnerHTML = div.innerHTML;
     if(instagram){
         curInnerHTML = highlight_instagram(curInnerHTML)
@@ -56,6 +69,8 @@ $(document).ready(function(){
     }
     div.innerHTML = curInnerHTML;
 }
+
+//Formatação de texto post twitter
 function highlight(words) {
     words = words.replace(/(^|\s)(@[^\w\d\s]*\w+)/gi, "$1<span class='mention'>$2</span>");
     words = words.replace(/(^|\s)(#[a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ_-]*)/gi, "$1<span class='hashtag'>$2</span>");
@@ -64,10 +79,26 @@ function highlight(words) {
     return words;
 }
 
+//Formatação de texto post instagram
 function highlight_instagram(words) {
     words = words.replace(/(^|\s)(@[^\w\d\s]*\w+)/gi, "$1<span class='mention_instagram'>$2</span>");
     words = words.replace(/(^|\s)(#[a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ_-]*)/gi, "$1<span class='hashtag_instagram'>$2</span>");
     words = words.replace(/(^|\s)(https?:\/\/[^\s,;!$%@*]+)/gi, "$1<span class='url'>$2</span>");
 
     return words;
-}*/
+}
+
+/*$("#baackgoundrss").css('background-image', 'url(' + urlimagerss +')').promise().done(function(){
+    $(this).animate({
+        opacity: 1
+    }, 600)
+});
+
+var bloco = $("#blocotransicao");
+    bloco.fadeOut(3000);
+        {top: "100%"}, 2500, function(){
+        $(this).hide()
+    }
+ );*/
+
+
