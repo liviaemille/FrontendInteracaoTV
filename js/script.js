b33d967afdf99d75f797c260a583b88a4de7f94b
Mauro Soares <mauro@tempocerto.inf.br>
2022-09-27 10:08:35 -0300

    :art:

--- js/script.js
+++ js/script.js
@@ -1,16 +1,15 @@
-function time(){
-    start()
+function time() {
 
-   //$(".logotransicao").animate({width: "30%"}, 1500, function(){
-     //   $(".blocotransicao").fadeOut(700)
+    //$(".logotransicao").animate({width: "30%"}, 1500, function(){
+    //   $(".blocotransicao").fadeOut(700)
     //});
 
-    const d = new Date();  
+    const d = new Date();
 
     //Dia da semana por extenso em portugues
     var dias = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
     dias = dias[d.getDay() % 7];
-    
+
     //Data em formato brasileiro
     data = d.toLocaleDateString();
 
@@ -20,63 +19,70 @@ function time(){
     segundo = d.getSeconds();
 
     //Formatação de horas, segundos e minutos para atribuir 0 a esquerda de cada um caso menores que 10
-    if(hora < 10){
-        hora="0"+hora;
+    if (hora < 10) {
+        hora = "0" + hora;
     }
-    if(minuto < 10){
-        minuto="0"+minuto;
+    if (minuto < 10) {
+        minuto = "0" + minuto;
     }
-    if(segundo < 10){
-        segundo="0"+segundo;
+    if (segundo < 10) {
+        segundo = "0" + segundo;
     }
 
     document.getElementById('diadata').innerHTML = (dias + ", " + data);
-    document.getElementById('hora').innerHTML = (hora+":"+minuto+":"+segundo);
-     
+    document.getElementById('hora').innerHTML = (hora + ":" + minuto + ":" + segundo);
+
     setTimeout('time()', 500);
 }
 
 //Função para procurar publicações dentro do body e iniciar animações e formatação de texto
-function start(){
-    $('body').find('.post').each(function(){
-      var div = this;
-    iniciarAnimation($(div));
+function iniciar() {
+    let posts = ["@livinhadograu  #Lorem Ipsum is simply dummy text of the printing and typesetting industry. ", "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s", "when an unknown printer took a galley of type and scrambled it to make a type specimen book."]
+
+    $('body').find('.post').each(function () {
+        var div = this;
+        iniciarAnimation($(div), posts);
+
     });
-
-   /* $('body').find('.muralnoticias').each(function(){
-        $('.titulonoticia').animate({left: '100%'}).hide()
-    })*/
-
-    $(document).ready(function(){
-        $()
-    })
+    setTimeout('iniciar()', 15000);
 }
 
+
 //Enviando os parametros certos para a função replace_text
-function iniciarAnimation(div){
+function iniciarAnimation(div, posts) {
     div.show();
     var img = "gatinho.jpg"
     //twitter
-    div.find('.text').each(function(){
-        replace_text(this,false);
+
+
+    current = posts.indexOf($("#twittexto").text()) + 1
+    if (current === -1) {
+        current = 0
+    }
+
+    div.find('.text').each(function () {
+        $("#twittexto").html(posts[current]);
+        replace_text(this, false);
     });
 
     //instagram
-    div.find('.textopost').each(function(){
-        replace_text(this,true);
+    div.find('.textopost').each(function () {
+        replace_text(this, true);
     });
     //$('body').find('.image').hide()
-    
+
     //.css({"background-image": "url(img/gatinho.jpg)"})
+
 }
 
 //Função para pegar conteúdo texto dos posts e enviar para as respectivas funções de formatação
-function replace_text(div,instagram){
+function replace_text(div, instagram) {
     var curInnerHTML = div.innerHTML;
-    if(instagram){
+
+    if (instagram) {
         curInnerHTML = highlight_instagram(curInnerHTML)
-    }else{
-      curInnerHTML = highlight(curInnerHTML)
+    } else {
+        curInnerHTML = highlight(curInnerHTML)
     }
     div.innerHTML = curInnerHTML;
 }
