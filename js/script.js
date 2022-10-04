8242ceb74de50f2d41ea1b23be8e7652dde6591e
Mauro Soares <mauro@tempocerto.inf.br>
2022-10-04 17:47:23 -0300

    :construction: add fadeIn

--- js/script.js
+++ js/script.js
@@ -36,52 +36,52 @@ function iniciar() {
     let usuarioTwitter = [{
         "Nome": "Livia Émille",
         "User": "@liviaemille",
-        "URLfoto" : "img/aslam.jpg",
-        "posts" : ["@livinhadograu  dlakjskdlajdf slajlaksjdklsdjaskdlkaj s", "ldkjaklsdjaljd alksdjlaksjd açlsdaçskdjaç sldkjaçsdlka", "#Lorem Ipsum is simply dummy text of the printing and typesetting industry."]
+        "URLfoto": "img/aslam.jpg",
+        "posts": ["@livinhadograu  dlakjskdlajdf slajlaksjdklsdjaskdlkaj s", "ldkjaklsdjaljd alksdjlaksjd açlsdaçskdjaç sldkjaçsdlka", "#Lorem Ipsum is simply dummy text of the printing and typesetting industry."]
     },
     {
         "Nome": "Mario Soares",
         "User": "@mariosoares",
-        "URLfoto" : "img/gatinho.jpg",
-        "posts" : ["Lorehgjm Ipsum hadhasjdaskldjalsdjas been lorem lkjsfk klsjflks jksdfkj jkdfkjsd kjdfksd fkjdsfs jdkfjksd jkdhfkjd <br> kshdaskjha sdhakjdhkjsldk", "the inaskdasjldldustry's standard dummy",  "text evelkasdjlkdsjaldjadr since the 1500s"]
-    }, 
+        "URLfoto": "img/gatinho.jpg",
+        "posts": ["Lorehgjm Ipsum hadhasjdaskldjalsdjas been lorem lkjsfk klsjflks jksdfkj jkdfkjsd kjdfksd fkjdsfs jdkfjksd jkdhfkjd <br> kshdaskjha sdhakjdhkjsldk", "the inaskdasjldldustry's standard dummy", "text evelkasdjlkdsjaldjadr since the 1500s"]
+    },
     {
         "Nome": "Maria Antônia",
         "User": "@mariaantonia",
         "URLfoto": "<img src='img/aslam.jpg' alt=''>",
-        "posts" : ["when an unknown printer took", "a galley of type and scrambled", "it to make a type specimen book."]
+        "posts": ["when an unknown printer took", "a galley of type and scrambled", "it to make a type specimen book."]
     }];
 
     let usuarioInstagram = [{
         "User": "@liviaemille",
-        "URLfoto" : "<img src='img/gatinho.jpg' alt=''>",
-        "textopub" : "@maria #hashtag ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus turpis id massa ultricies fringilla. Cras massa dolor, tempus nec ullamcorper at, consequat sed odio. Vivamus vel purus luctus, sollicitudin mi ac, luctus justo."
+        "URLfoto": "<img src='img/gatinho.jpg' alt=''>",
+        "textopub": "@maria #hashtag ipsum dolor sit amet, consectetur adipiscing elit. Fusce faucibus turpis id massa ultricies fringilla. Cras massa dolor, tempus nec ullamcorper at, consequat sed odio. Vivamus vel purus luctus, sollicitudin mi ac, luctus justo."
     },
     {
-        "User" : "@mariosoares", 
-        "URLfoto" : "<img src='img/qrcodeteste.png' alt=''>",
+        "User": "@mariosoares",
+        "URLfoto": "<img src='img/qrcodeteste.png' alt=''>",
         "textopub": "Mauris consequat molestie leo id efficitur. Cras fermentum, quam ac fermentum gravida, tortor elit tempus sem, nec gravida risus lacus non arcu. Nullam pellentesque, nisi eget laoreet scelerisque, lorem nulla suscipit eros, quis aliquam leo nisi a eros."
-        
+
     },
     {
-        "User":"@toinhabigshoes",
+        "User": "@toinhabigshoes",
         "URLfoto": "<img src='img/paisagem.jpg' alt=''>",
-        "textopub" : "Cras massa dolor, tempus nec ullamcorper at, consequat sed odio. Vivamus vel purus luctus, sollicitudin mi ac, luctus justo. Mauris consequat molestie leo id efficitur. Cras fermentum, quam ac fermentum gravida, tortor elit tempus sem, nec gravida risus lacus non arcu."
+        "textopub": "Cras massa dolor, tempus nec ullamcorper at, consequat sed odio. Vivamus vel purus luctus, sollicitudin mi ac, luctus justo. Mauris consequat molestie leo id efficitur. Cras fermentum, quam ac fermentum gravida, tortor elit tempus sem, nec gravida risus lacus non arcu."
     }];
 
     let noticiasRSS = [{
         "Titulo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "Conteudo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         "Background": "img/paisagem.jpg",
-        "QRcode" : "<img src='img/aslam.jpg' alt=''>"
+        "QRcode": "<img src='img/aslam.jpg' alt=''>"
     },
     {
-        "Titulo" : "Morbi ullamcorper leo mi, non vulputate ipsum volutpat et.",
-        "Conteudo" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "Background" : "img/paisagem2.jpg",
-        "QRcode" : "<img src='img/qrcodeteste.png' alt=''>"
+        "Titulo": "Morbi ullamcorper leo mi, non vulputate ipsum volutpat et.",
+        "Conteudo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Background": "img/paisagem2.jpg",
+        "QRcode": "<img src='img/qrcodeteste.png' alt=''>"
     }
-];
+    ];
     $('body').find('.conteiner').each(function () {
         let div = this;
         let indicerss = Math.floor(Math.random() * noticiasRSS.length);
@@ -96,89 +96,98 @@ function iniciar() {
 
 //Enviando os parametros certos para a função replace_text
 function iniciarAnimation(div, objt, indicet, obji, indicei, objr, indicer) {
-    div.show();
-    // Trata postagens do twitter
-    div.find(".muraltwitter").each(function(){
-        $("#username").html(objt[indicet]["User"]);
-        $("#nome_do_usuario").html(objt[indicet]["Nome"]);
-        $("#twittexto").html(objt[indicet]["posts"][Math.floor(Math.random() * objt[indicet]["posts"].length)]);
-        $('#fotoperfil').css("background-image", "url("+objt[indicet]['URLfoto']+")");
+     div.show();
+    $(".perfil").hide()
+    $(".tweet").hide()
 
-        div.find('#twittexto').each(function(){
-        replace_text(this, false);
+    $(".perfil").fadeIn(2000)
+    $(".tweet").fadeIn(2000)
+
+    // Trata postagens do twitter
+    div.find(".muraltwitter").each(function () {
+        $("#username").html(objt[indicet]["User"])
+        $("#nome_do_usuario").html(objt[indicet]["Nome"])
+        $("#twittexto").html(objt[indicet]["posts"][Math.floor(Math.random() * objt[indicet]["posts"].length)])
+        $('#fotoperfil').css("background-image", "url(" + objt[indicet]['URLfoto'] + ")")
+
+        div.find('#twittexto').each(function () {
+            replace_text(this, false);
         })
     })
 
+
     // Trata postagens do instagram
 
-    div.find(".mural").each(function(){
+    div.find(".mural").each(function () {
         $("#image").html(obji[indicei]["URLfoto"]);
         $('#user').html(obji[indicei]["User"]);
         $('#instatexto').html(obji[indicei]["textopub"]);
-        
+
         div.find('#instatexto').each(function () {
-        replace_text(this, true);
-        });
+            replace_text(this, true);
+        })
     })
-    
+
 
     // Transições RSS
-    div.find('.titulonoticia').each(function(){
-            $(this).delay(1000).animate(
-                {left: '500px'},
-                {opacity: 1.0},
-                {duration : 5000}
-            )
+    div.find('.titulonoticia').each(function () {
+        $(this).delay(1000).animate(
+            { left: '500px' },
+            { opacity: 1.0 },
+            { duration: 5000 }
+        )
     })
 
-    div.find('.muralnoticias').each(function(){
-        $('#titulonoticia').each(function(){
+    div.find('.muralnoticias').each(function () {
+        $('#titulonoticia').each(function () {
             $(this).html(objr[indicer]["Titulo"]);
-            
+
         });
         $('#links').html(objr[indicer]["Conteudo"]);
         $('.qrcode').html(objr[indicer]['QRcode'])
-        $('.bgrss').css("background-image", "url("+objr[indicer]["Background"]+")")
+        $('.bgrss').css("background-image", "url(" + objr[indicer]["Background"] + ")")
     })
 
-    div.find('#image').each(function(){
+    div.find('#image').each(function () {
         $(this).animate(
-            {left : '250px'},
-            {opacity : 1.0}
+            { left: '250px' },
+            { opacity: 1.0 }
         )
     })
-    div.find('#blocotransicao').each(function(){
+    div.find('#blocotransicao').each(function () {
         $(this).slideUp(slow);
         $(this).hide();
     })
 
 
-//Função para pegar conteúdo texto dos posts e enviar para as respectivas funções de formatação
-function replace_text(div, instagram) {
-    var curInnerHTML = div.innerHTML;
+    //Função para pegar conteúdo texto dos posts e enviar para as respectivas funções de formatação
+    function replace_text(div, instagram) {
+        var curInnerHTML = div.innerHTML;
 
-    if (instagram) {
-        curInnerHTML = highlight_instagram(curInnerHTML)
-    } else {
-        curInnerHTML = highlight(curInnerHTML)
+        if (instagram) {
+            curInnerHTML = highlight_instagram(curInnerHTML)
+        } else {
+            curInnerHTML = highlight(curInnerHTML)
+        }
+        div.innerHTML = curInnerHTML;
     }
-    div.innerHTML = curInnerHTML;
+
+    //Formatação de texto post twitter
+    function highlight(words) {
+        words = words.replace(/(^|\s)(@[^\w\d\s]*\w+)/gi, "$1<span class='mention'>$2</span>");
+        words = words.replace(/(^|\s)(#[a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ_-]*)/gi, "$1<span class='hashtag'>$2</span>");
+        words = words.replace(/(^|\s)(https?:\/\/[^\s,;!$%@*]+)/gi, "$1<span class='url'>$2</span>");
+
+        return words;
+    }
+
+    //Formatação de texto post instagram
+    function highlight_instagram(words) {
+        words = words.replace(/(^|\s)(@[^\w\d\s]*\w+)/gi, "$1<span class='mention_instagram'>$2</span>");
+        words = words.replace(/(^|\s)(#[a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ_-]*)/gi, "$1<span class='hashtag_instagram'>$2</span>");
+        words = words.replace(/(^|\s)(https?:\/\/[^\s,;!$%@*]+)/gi, "$1<span class='url'>$2</span>");
+
+        return words;
+    }
+
 }
-
-//Formatação de texto post twitter
-function highlight(words) {
-    words = words.replace(/(^|\s)(@[^\w\d\s]*\w+)/gi, "$1<span class='mention'>$2</span>");
-    words = words.replace(/(^|\s)(#[a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ_-]*)/gi, "$1<span class='hashtag'>$2</span>");
-    words = words.replace(/(^|\s)(https?:\/\/[^\s,;!$%@*]+)/gi, "$1<span class='url'>$2</span>");
-
-    return words;
-}
-
-//Formatação de texto post instagram
-function highlight_instagram(words) {
-    words = words.replace(/(^|\s)(@[^\w\d\s]*\w+)/gi, "$1<span class='mention_instagram'>$2</span>");
-    words = words.replace(/(^|\s)(#[a-zA-Z0-9ÀÁÂÃÄÅàáâãäåÒÓÔÕÖØòóôõöøÈÉÊËèéêëÇçÌÍÎÏìíîïÙÚÛÜùúûüÿÑñ_-]*)/gi, "$1<span class='hashtag_instagram'>$2</span>");
-    words = words.replace(/(^|\s)(https?:\/\/[^\s,;!$%@*]+)/gi, "$1<span class='url'>$2</span>");
-
-    return words;
-}}
